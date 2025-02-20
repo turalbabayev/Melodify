9e9e9beede25a93628251a156c63c137f750b718
turalbabayev <turalbabayev@outlook.com.tr>
2025-02-20 17:46:36 +0300

    Initial Commit

---
+++ Melodify/MelodifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelodifyApp.swift
+//  Melodify
+//
+//  Created by Tural Babayev on 20.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MelodifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
