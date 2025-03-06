b39a4af060d54d86c132e8bff352e0ff03f97c7d
turalbabayev <turalbabayev@outlook.com.tr>
2025-03-06 23:51:35 +0300

    new features

--- Melodify/Core/Features/MusicGenerator/View/ComposeView.swift
+++ Melodify/Core/Features/MusicGenerator/View/ComposeView.swift
@@ -8,13 +8,14 @@
 import SwiftUI
 
 struct ComposeView: View {
+    @Namespace private var animation // Animasyon için namespace
     @State private var isExpanded: Bool = true
     @ObservedObject var viewModel: MusicGeneratorViewModel
 
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
             HStack {
-                Text("Prompt")
+                Text("Lyrics")
                     .font(.system(size: 14))
                     .fontWeight(.bold)
                     .foregroundColor(.white)
@@ -33,22 +34,35 @@ struct ComposeView: View {
             .padding(.horizontal, 16)
             .padding(.bottom, 5)
 
-            
             if isExpanded {
                 VStack(alignment: .leading, spacing: 16) {
-                    CustomTextEditor(text: $viewModel.prompt.text, placeholder: "Bir şeyler yazın...")
-                        .padding(.bottom, 10)
+                    if !viewModel.prompt.isInstrumental {
+                        CustomTextEditor(text: $viewModel.prompt.text, placeholder: "Bir şeyler yazın...")
+                            .padding(.bottom, 10)
+                    } else {
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(Color.purple.opacity(0.7))
+                            .frame(height: 50)
+                            .padding(.horizontal, 16)
+                            .overlay(
+                                Text("Instrumental modda yazı yazamazsınız.")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 14))
+                                    .padding()
+                            )
+                            .transition(.scale) // Ölçek animasyonu
+                            .animation(.easeInOut, value: viewModel.prompt.isInstrumental)
+                    }
                     
                     Toggle("Instrumental", isOn: $viewModel.prompt.isInstrumental)
                         .toggleStyle(SwitchToggleStyle(tint: Color.purple))
                         .padding(.horizontal, 16)
                 }
+                .transition(.opacity)
+                .animation(.easeInOut, value: isExpanded)
             }
-            
-            
         }
         .padding(.vertical)
-
     }
 }
 
